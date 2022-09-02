fd4656a7d946ded701fd62c575ece5dd8abb44b1
joy <joy@gmail.com>
2022-09-02 15:23:09 +0800

    fix bugs

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,8 +2,8 @@
 #判断是否初始化
 
 # look for empty dir
-if [ "ls -A /app/sub-web/src/views" = "" ]; then
-    cp -Rf /var/dev/views /app/sub-web/src/views
+if [ -z "$(ls -A /app/sub-web/src/views)"]; then
+    cp -rf /var/dev/views/* /app/sub-web/src/views
 fi
 
 cd /app/sub-web && yarn serve &
