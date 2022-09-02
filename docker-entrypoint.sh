69064ad680cf71cf15c4f8fbf21b7965382b9a53
joy <joy@gmail.com>
2022-09-02 14:35:58 +0800

    自动创建依赖

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
+#判断是否初始化
+
+# look for empty dir
+if [ "ls -A /app/sub-web/src/views" = "" ]; then
+    cp -Rf /app/dev/views /app/sub-web/src/views
+fi
+
 cd /app/sub-web && yarn serve &
 /usr/sbin/nginx
 /var/dev/subconverter/subconverter
